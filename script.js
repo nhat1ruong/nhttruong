58e6afeb45693295580ddcadbf0b7890bb0246a4
nhat1ruong <nguyennhattruong10m@gmail.com>
2025-02-21 10:12:26 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=devica-width">
+   <title> BÀI TẬP TIN HỌC 12</title>
+   <link href="style.css" rel="stylesheet" type-"text/css" /›
+</head>
+<body>
+  <form>
+     <label> Tên đăng nhập:</label>
+     <input type="text" id="username" name="username">
+     <label> Mật khẩu:</label>
+     <input type="password" id="pw" name="pw">
+     <button type="submit" id="ok" name="ok" onclick=login() > ĐĂNG NHẬP </button>
+  </form>
+  <script></script>
+  </body>
+  </html>
