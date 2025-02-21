481158f3322f2fa4c0173efeb338e221fd3a96c8
nhat1ruong <nguyennhattruong10m@gmail.com>
2025-02-21 10:19:10 +0700

    Create Create script.js

---
+++ Create script.js
@@ -0,0 +1,9 @@
+functuon login()
+{
+   let u = username.value
+   let p = pw.value
+   if (u=="thao" && p=="man")
+   {
+     window.open("home.html")
+     document getElementById(compiler).style.display="block"
+   }
